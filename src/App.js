2ccc5208f87ab2e3bac30b5b202f35a7b67d6230
Rafal <rafal.szczurekk@gmail.com>
2021-05-17 22:21:49 +0200

    xx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [state, setstate] = useState([]);
+  useEffect(() => {
+    updateList()
+  }, []);
+  const updateList = () => {
+    let config = {
+      headers: {
+        ApiKij: "12nfhfkjaha983ZKsakjh12989S11",
+      },
+    };
+
+    axios
+      .get(
+        `https://weddingonline-test.azurewebsites.net/api/wedding/ŚlubMoniki_i_kacpra`,
+        config
+      )
+      .then((response) => {
+        console.log(response);
+        setstate(response.data.guests);
+      })
+      .catch((error) => {
+        console.log('error');
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <table>
+    
+   
+    {state.map((iteam) => (
+      <tr>
+      <td>{iteam.firstName}</td> <td>{iteam.lastName}</td> <td>{iteam.decisionStatus == 3 ? 'Potwierdzony' : null} {iteam.decisionStatus == 2 ? 'Może' : null} {iteam.decisionStatus == 1 ? 'Nie będe' : null} {iteam.decisionStatus == 0 ? 'Nie zaznaczył' : null}</td>
+      </tr>
+    ))}
+    </table>
     </div>
   );
 }
